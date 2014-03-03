9ac229651dd2cdea3568522b22584ef2f5513c92
Apache <apache@ser01.softobiz.com>
2014-03-03 07:09:35 +0000

    First Commit By Chance

---
+++ jquery.youtube.channel.js
@@ -0,0 +1,76 @@
+var __mainDiv;
+ var __preLoaderHTML;
+ var __opts;
+
+ function __jQueryYouTubeChannelReceiveData(data,deviceType) {
+
+     var cnt = 0;
+     $.each(data.feed.entry, function(i, e) {
+         if (cnt < __opts.numberToDisplay) {
+             var vidUrl=e.link[0].href;
+             vidUrl=(vidUrl.replace(/www.youtube.com/gi, "m.youtube.com"));
+             var parts = e.id.$t.split('/');
+             var videoId = parts[parts.length-1];
+		
+             var out = '<li class="ui-li ui-li-static ui-body-c"><center><p class="ui-li-desc"><br><a href="' + 
+             vidUrl + '" rel="external" class="open-plugin-videoplayer" data-vidurl="'+vidUrl+'"><img src="http://i.ytimg.com/vi/' + 
+             videoId + '/2.jpg"/ width="240px" height="180px"></a></p><p class="ui-li-desc"><a href="' + 
+             vidUrl + '" rel="external" class="open-plugin-videoplayer" data-vidurl="'+vidUrl+'">' + e.title.$t + '</a></p></center></li>';
+			 
+		
+             if (!__opts.hideAuthor) {
+                 out = out + 'Author: ' + e.author[0].name.$t + '';
+             }
+             out = out + '</center></p>';
+             __mainDiv.append(out);
+             cnt = cnt + 1;
+         }
+     });
+            
+    // Open in new tab?
+    if (__opts.linksInNewWindow) {
+        $(__mainDiv).find("li > a").attr("target", "_blank");
+    }
+    
+    // Remove the preloader and show the content
+    $(__preLoaderHTML).remove();
+    __mainDiv.show();
+}
+                
+(function($) {
+    $.fn.youTubeChannel = function(options) {
+        var videoDiv = $(this);
+
+        $.fn.youTubeChannel.defaults = {
+            userName: null,
+            channel: "uploads", //options are favorites or uploads
+            loadingText: "Loading...",
+            numberToDisplay: 3,
+            linksInNewWindow: true,
+            hideAuthor: false
+        }
+
+        __opts = $.extend({}, $.fn.youTubeChannel.defaults, options);
+
+        return this.each(function() {
+            if (__opts.userName != null) {
+                videoDiv.append("<ul id=\"linksList\" class=\"ui-listview ui-listview-inset ui-corner-all ui-shadow\" data-inset=\"true\" data-role=\"listview\"></ul>");
+                __mainDiv = $("#linksList");
+                __mainDiv.hide();
+
+                __preLoaderHTML = $("<p class=\"loader\">" + 
+                    __opts.loadingText + "</p>");
+                videoDiv.append(__preLoaderHTML);
+
+                // TODO: Error handling!
+                $.ajax({
+                    url: "http://gdata.youtube.com/feeds/base/users/" + 
+                        __opts.userName + "/" + __opts.channel + "?alt=json",
+                    cache: true,
+                    dataType: 'jsonp',                    
+                    success: __jQueryYouTubeChannelReceiveData
+                });
+            }
+        });
+    };
+})(jQuery);
